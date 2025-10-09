97210cedd8a4ba412e6591ebeabfa3011eceedf3
saruni-spec <sarunimaina@gmail.com>
2025-10-09 14:19:39 +0300

    hope

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  reactStrictMode: true,
 }
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default withPayload(nextConfig, {
+  configPath: './src/payload.config.ts', // 👈 tell Payload where your config is
+  devBundleServerPackages: false,
+})

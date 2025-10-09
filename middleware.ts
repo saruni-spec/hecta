25dec26c16ae1a30850b79d93089712a3531af87
saruni-spec <sarunimaina@gmail.com>
2025-10-09 13:29:32 +0300

    middleware

---
+++ middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from 'next/server'
+
+const allowed_origins = [
+  'https://hectaconsulting.com',
+  'https://www.hectaconsulting.com',
+  'http://localhost:3000',
+  'http://127.0.0.1:5500',
+  'http://localhost:5500',
+]
+
+export function middleware(req: Request) {
+  const origin = req.headers.get('origin')
+  const res = NextResponse.next()
+
+  if (origin && allowed_origins.includes(origin)) {
+    res.headers.set('Access-Control-Allow-Origin', origin)
+    res.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    res.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  }
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return new Response(null, { headers: res.headers })
+  }
+
+  return res
+}
